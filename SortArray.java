5ed7136112fb86cf3ee6bf4dfc3a7642a03ed953
tson236 <tsphan236@gmail.com>
2026-03-06 19:53:55 +0700

    Update SortArray.java

--- SortArray.java
+++ SortArray.java
@@ -1,37 +1,26 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class SortArray {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter number of elements: ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
-
-        // nhập mảng
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             System.out.print("Enter element " + (i + 1) + ": ");
             arr[i] = sc.nextInt();
         }
-
-        // tính tổng
         int sum = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             sum += arr[i];
         }
-
-        // tính trung bình
         double avg = (double) sum / n;
-
-        // sắp xếp mảng
         Arrays.sort(arr);
-
-        // hiển thị kết quả
         System.out.println("Sorted array: " + Arrays.toString(arr));
         System.out.println("Sum = " + sum);
         System.out.println("Average = " + avg);
     }
+
 }
